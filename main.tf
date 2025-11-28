e42c5e9cb42de01e9fc830730206765dae3a1a9e
venkey <subramanymsubbu813@gmail.com>
2025-11-28 09:28:09 +0530

    Pusing all the files into GitHub

---
+++ main.tf
@@ -0,0 +1,62 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "venkat-vpc"
+  }
+}
+resource "aws_subnet" "main_public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public subnet"
+  }
+}
+resource "aws_subnet" "main_private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "private subnet"
+  }
+
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "igw"
+  }
+}
+ resource"aws_route_table" "venkey_public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "venkey_public"
+  }
+} 
+
+resource "aws_route_table" "venkey_private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "venkey_private"
+  }
+}
+
+# 
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.main_public.id
+  route_table_id = aws_route_table.venkey_public.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.main_private.id
+  route_table_id = aws_route_table.venkey_private.id
+}
